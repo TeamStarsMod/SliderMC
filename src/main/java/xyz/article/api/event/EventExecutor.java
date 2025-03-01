97aae891d3fe49ff2420739e53678ab4d1640610
Neko110923 <neko110923@163.com>
2025-03-01 20:46:17 +0800

    更新

---
+++ src/main/java/xyz/article/api/event/EventExecutor.java
@@ -0,0 +1,13 @@
+package xyz.article.api.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventExecutor {
+    int priority() default 1;
+    boolean ignoreCancelled() default false;
+}
--- src/main/java/xyz/article/api/inventory/PlayerInventory.java
+++ src/main/java/xyz/article/api/inventory/PlayerInventory.java
@@ -1,4 +1,7 @@
 package xyz.article.api.inventory;
 
 public class PlayerInventory extends Inventory {
+    public PlayerInventory() {
+        super(0, 46, null);
+    }
 }
