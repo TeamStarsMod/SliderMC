5600beb8900cf76e27d0c6bd68336c5841f414bf
cyhcyh2 <1619769726@qq.com>
2025-03-02 13:53:06 +0800

    加了一个小api

--- src/main/java/xyz/article/api/Slider.java
+++ src/main/java/xyz/article/api/Slider.java
@@ -1,6 +1,7 @@
 package xyz.article.api;
 
 import org.geysermc.mcprotocollib.network.Session;
+import org.jetbrains.annotations.Nullable;
 import xyz.article.RunningData;
 import xyz.article.api.entities.player.Player;
 
@@ -10,7 +11,20 @@ public class Slider {
      * @param session 用于获取玩家的Session实例
      * @return 获取到的玩家实例
      */
-    public static Player getPlayer(Session session) {
+    public static @Nullable Player getPlayer (Session session) {
         return RunningData.globalSessionPlayerMap.get(session);
+    }
+
+    /**
+     * 获取玩家实例（通过玩家名或UUID）
+     * @param name 玩家名或UUID
+     * @return 获取到的玩家实例
+     */
+    public static @Nullable Player getPlayer (String name) {
+        for (Player globalPlayer : RunningData.globalPlayers) {
+            if (globalPlayer.getProfile().getName().equals(name)) return globalPlayer;
+            else if (globalPlayer.getProfile().getId().toString().equals(name)) return globalPlayer;
+        }
+        return null;
     }
 }
